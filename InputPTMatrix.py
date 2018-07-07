e779a431d97871a49ce9abd61ce77a18b8920b41
Tejas Sanap <tsanap@icarus.mshome.net>
2018-07-07 16:33:11 +0530

    This program is written to take PT matrix as input for n jobs/items and m machines. The program takes input over console. It takes input in a tabular format.

--- InputPTMatrix.py
+++ InputPTMatrix.py
@@ -1,3 +1,17 @@
+# Output:
+# 
+# Machines: 3
+# Items: 5
+# 	  M/c 1 | M/c 2 | M/c 3 |
+# ------------------------------------
+# Job1	| 16	18	12
+# Job2	| 14	10	11
+# Job3	| 13	20	15
+# Job4	| 19	15	19
+# Job5	| 15	16	16
+# 
+# [[16, 18, 12], [14, 10, 11], [13, 20, 15], [19, 15, 19], [15, 16, 16]]
+
 def main():
     machines=int(input("Machines: "))
     items=int(input("Items: "))
